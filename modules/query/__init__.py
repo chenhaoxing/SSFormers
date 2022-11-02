db08bde6392eb374a1cf17c7aa779c252e23be43
chx_nju <58558762+chenhaoxing@users.noreply.github.com>
2022-11-02 11:55:51 +0800

    Update __init__.py

--- modules/query/__init__.py
+++ modules/query/__init__.py
@@ -3,6 +3,7 @@ from modules.query.protonet import *
 from modules.query.matchingnet import *
 from modules.query.st import *
 from modules.query.dn4 import *
+from modules.query.ctx import *
 import modules.registry as registry
 
 def make_query(in_channels, cfg):
